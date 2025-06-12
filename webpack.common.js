1135c6f8a51ebd9e3c64485f47cf0a09ab5cc637
Aditia Prabowo <aditiaprabowo3@gmail.com>
2025-06-12 12:26:16 +0700

    update

--- webpack.common.js
+++ webpack.common.js
@@ -1,84 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const BundleAnalyzerPlugin =
-    require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+/* eslint-disable array-bracket-spacing */
+/* eslint-disable comma-dangle */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: path.resolve(__dirname, "src/scripts/index.js"),
+        app: path.resolve(__dirname, 'src/scripts/index.js'),
     },
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     module: {
         rules: [{
             test: /\.css$/,
             use: [{
-                    loader: "style-loader",
+                    loader: 'style-loader',
                 },
                 {
-                    loader: "css-loader",
+                    loader: 'css-loader',
+                },
+                {
+                    loader: 'postcss-loader',
                 },
             ],
         }, ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-            minSize: 20000,
-            maxSize: 70000,
-            minChunks: 1,
-            maxAsyncRequests: 30,
-            maxInitialRequests: 30,
-            automaticNameDelimiter: "~",
-            enforceSizeThreshold: 50000,
-            cacheGroups: {
-                defaultVendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
-        },
     },
     plugins: [
         new CleanWebpackPlugin(),
 
         new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve(__dirname, "src/templates/index.html"),
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/templates/index.html'),
         }),
-
         new CopyWebpackPlugin({
             patterns: [{
-                from: path.resolve(__dirname, "src/public"),
-                to: path.resolve(__dirname, "dist"),
-                //   globOptions: {
-                //     ignore: ["**/heros/**"],
-                //   },
+                from: path.resolve(__dirname, 'src/public/'),
+                to: path.resolve(__dirname, 'dist/'),
             }, ],
         }),
-
-        new ImageMinimizerPlugin({
-            minimizer: {
-              implementation: ImageMinimizerPlugin.imageminGenerate,
-              options: {
-                plugins: [
-                  ["mozjpeg", { quality: 50, progressive: true }],
-                ],
-              },
-            },
-        }),      
-
-        new BundleAnalyzerPlugin(),
     ],
 };
